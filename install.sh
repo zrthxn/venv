a9bf2782679a247e04b6e9e315acb80deb358616
Alisamar Husain <zrthxn@gmail.com>
2021-05-28 16:36:07 +0530

    Install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-sudo apt-get update
-sudo apt-get install python-virtualenv
+# sudo apt-get update
+# sudo apt-get install python-virtualenv
 
 # Download
-wget https://raw.githubusercontent.com/zrthxn/venv/master/venv.sh > /usr/local/sbin/venv.sh
-sudo chmod +x /usr/local/sbin/venv.sh
+sudo wget https://raw.githubusercontent.com/zrthxn/venv/master/venv.sh -O /usr/local/bin/venv
+sudo ln -s /usr/local/bin/venv /usr/bin
+sudo chmod +x /usr/local/bin/venv
